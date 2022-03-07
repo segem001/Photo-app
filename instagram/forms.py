b63adea1676b1835c345649537b72673113b01e4
segem001 <kipdom001@gmail.com>
2022-03-07 20:09:47 +0300

    Add forms model

--- instagram/forms.py
+++ instagram/forms.py
@@ -26,3 +26,18 @@ class UpdateUserProfileForm(forms.ModelForm):
         fields = ['name', 'location', 'profile_picture', 'bio']
 
 
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ('image', 'caption')
+
+
+class CommentForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['comment'].widget = forms.TextInput()
+        self.fields['comment'].widget.attrs['placeholder'] = 'Add a comment...'
+
+    class Meta:
+        model = Comment
+        fields = ('comment',)
